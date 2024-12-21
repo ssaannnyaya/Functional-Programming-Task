7dc803ec80df87b3d52599db1610a297b14aea55
ssaannnyaya <sanek_lukyanov@inbox.ru>
2024-12-21 19:42:00 +0300

    Improve Gauss functions graph

--- src/main/java/ru/ac/uniyar/Simplex/Utils/Gauss.java
+++ src/main/java/ru/ac/uniyar/Simplex/Utils/Gauss.java
@@ -29,19 +29,22 @@ public class Gauss {
         return newTable;
     }
 
+    static public int getNonZeroRow(Fraction[][] table, int m, int startRow, int j) {
+        int row = startRow;
+        while (row < m - 1 && Fraction.equals(table[row][j], Fraction.zero())) {
+            row++;
+        }
+        return row;
+    }
+
     static public Fraction[][] calculateByVars(Fraction[][] table, int n, int m, int[] vars) {
         Fraction[][] newTable = table.clone();
         for (int j = 0; j < vars.length; j++) {
-            int row = j;
-            while (row < m - 1 && Fraction.equals(newTable[row][vars[j]], Fraction.zero())) {
-                row++;
-            }
+            int row = getNonZeroRow(table, m, j, vars[j]);
             if (!Fraction.equals(newTable[row][vars[j]], Fraction.zero())) {
                 newTable = divideRow(newTable, n, row, newTable[row][vars[j]]);
-                for (int i = 0; i < row; i++) {
-                    newTable = subtractionRowsWithMultiplier(newTable, n, row, i, newTable[i][vars[j]]);
-                }
-                for (int i = row + 1; i < m; i++) {
+                for (int i = 0; i < m; i++) {
+                    if (i == row) continue;
                     newTable = subtractionRowsWithMultiplier(newTable, n, row, i, newTable[i][vars[j]]);
                 }
                 newTable = switchRows(newTable, n, row, j);
@@ -50,21 +53,25 @@ public class Gauss {
         return newTable;
     }
 
-    static public Fraction[][] transformTable(Fraction[][] table, int n, int m, int[] vars) {
-        Fraction[][] newTable = table.clone();
-        newTable = calculateByVars(newTable, n, m, vars);
-        Fraction[][] transformedTable = new Fraction[m + 1][n - vars.length + 1];
+    static public Fraction[][] getTableWithoutVars(Fraction[][] table, int n, int m, int[] vars) {
+        Fraction[][] newTable = new Fraction[m + 1][n - vars.length + 1];
         int offset = 0;
         for (int j = 0; j <= n; j++) {
             int J = j;
-            if (Arrays.stream(vars).filter(it -> it == J).toArray().length != 0) {
+            if (Arrays.stream(vars).anyMatch(it -> it == J)) {
                 offset++;
             } else {
                 for (int i = 0; i < m; i++) {
-                    transformedTable[i][j - offset] = newTable[i][j];
+                    newTable[i][j - offset] = table[i][j];
                 }
             }
         }
-        return transformedTable;
+        return newTable;
+    }
+
+    static public Fraction[][] transformTable(Fraction[][] table, int n, int m, int[] vars) {
+        Fraction[][] newTable = table.clone();
+        newTable = calculateByVars(newTable, n, m, vars);
+        return getTableWithoutVars(newTable, n, m, vars);
     }
 }
